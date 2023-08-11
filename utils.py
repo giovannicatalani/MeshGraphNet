4f7834ab9404343736111a0bfae8df397b909c7e
Giovanni Catalani <giovannicatalani98@gmail.com>
2023-08-11 19:12:42 +0200

    aa

---
+++ utils.py
@@ -0,0 +1,150 @@
+import tensorflow as tf
+import torch
+import torch.optim as optim
+from torch_geometric.data import Data
+
+def triangles_to_edges(faces):
+  """Computes mesh edges from triangles.
+     Note that this triangles_to_edges method was provided as part of the
+     code release for the MeshGraphNets paper by DeepMind, available here:
+     https://github.com/deepmind/deepmind-research/tree/master/meshgraphnets
+  """
+  # collect edges from triangles
+  edges = tf.concat([faces[:, 0:2],
+                     faces[:, 1:3],
+                     tf.stack([faces[:, 2], faces[:, 0]], axis=1)], axis=0)
+  # those edges are sometimes duplicated (within the mesh) and sometimes
+  # single (at the mesh boundary).
+  # sort & pack edges as single tf.int64
+  receivers = tf.reduce_min(edges, axis=1)
+  senders = tf.reduce_max(edges, axis=1)
+  packed_edges = tf.bitcast(tf.stack([senders, receivers], axis=1), tf.int64)
+  # remove duplicates and unpack
+  unique_edges = tf.bitcast(tf.unique(packed_edges)[0], tf.int32)
+  senders, receivers = tf.unstack(unique_edges, axis=1)
+  # create two-way connectivity
+  return (tf.concat([senders, receivers], axis=0),
+          tf.concat([receivers, senders], axis=0))
+
+
+def create_dataset(data):
+    #Define the list that will return the data graphs
+    data_list = []
+
+    for ts in range(len(data['V'])):
+
+        #Get node features
+        momentum = torch.full((len(data['nodes']),1), data['V'][ts])
+        alpha = torch.full((len(data['nodes']),1), data['alpha'][ts])
+        x = torch.cat((momentum,alpha),dim=-1).type(torch.float)
+
+        #Get edge indices in COO format
+        edges = triangles_to_edges(tf.convert_to_tensor(data['triangles']))
+        edge_index = torch.cat( (torch.tensor(edges[0].numpy()).unsqueeze(0) ,
+                            torch.tensor(edges[1].numpy()).unsqueeze(0)), dim=0).type(torch.long)
+
+        #Get edge features
+        u_i=torch.tensor(data['nodes'])[edge_index[0]]
+        u_j=torch.tensor(data['nodes'])[edge_index[1]]
+        u_ij=u_i-u_j
+        u_ij_norm = torch.norm(u_ij,p=2,dim=1,keepdim=True)
+        edge_attr = torch.cat((u_ij,u_ij_norm),dim=-1).type(torch.float)
+
+        #Node outputs, for training (pressure)
+        y=torch.tensor(data['pressure'][ts]).unsqueeze(1).type(torch.float)
+        
+        data_list.append(Data(x=x, edge_index=edge_index, edge_attr=edge_attr,y=y))
+
+    print("Done collecting data!")
+
+    return data_list
+
+def normalize(to_normalize,mean_vec,std_vec):
+    return (to_normalize-mean_vec)/std_vec
+
+def unnormalize(to_unnormalize,mean_vec,std_vec):
+    return to_unnormalize*std_vec+mean_vec
+
+def get_stats(data_list):
+    '''
+    Method for normalizing processed datasets. Given  the processed data_list, 
+    calculates the mean and standard deviation for the node features, edge features, 
+    and node outputs, and normalizes these using the calculated statistics.
+    '''
+
+    #mean and std of the node features are calculated
+    mean_vec_x=torch.zeros(data_list[0].x.shape[1:])
+    std_vec_x=torch.zeros(data_list[0].x.shape[1:])
+
+    #mean and std of the edge features are calculated
+    mean_vec_edge=torch.zeros(data_list[0].edge_attr.shape[1:])
+    std_vec_edge=torch.zeros(data_list[0].edge_attr.shape[1:])
+
+    #mean and std of the output parameters are calculated
+    mean_vec_y=torch.zeros(data_list[0].y.shape[1:])
+    std_vec_y=torch.zeros(data_list[0].y.shape[1:])
+
+    #Define the maximum number of accumulations to perform such that we do
+    #not encounter memory issues
+    max_accumulations = 10**6
+
+    #Define a very small value for normalizing to 
+    eps=torch.tensor(1e-8)
+
+    #Define counters used in normalization
+    num_accs_x = 0
+    num_accs_edge=0
+    num_accs_y=0
+
+    #Iterate through the data in the list to accumulate statistics
+    for dp in data_list:
+
+        #Add to the 
+        mean_vec_x+=torch.sum(dp.x,dim=0)
+        std_vec_x+=torch.sum(dp.x**2,dim=0)
+        num_accs_x+=dp.x.shape[0]
+
+        mean_vec_edge+=torch.sum(dp.edge_attr,dim=0)
+        std_vec_edge+=torch.sum(dp.edge_attr**2,dim=0)
+        num_accs_edge+=dp.edge_attr.shape[0]
+
+        mean_vec_y+=torch.sum(dp.y,dim=0)
+        std_vec_y+=torch.sum(dp.y**2,dim=0)
+        num_accs_y+=dp.y.shape[0]
+
+        if(num_accs_x>max_accumulations or num_accs_edge>max_accumulations or num_accs_y>max_accumulations):
+            break
+
+    mean_vec_x = mean_vec_x/num_accs_x
+    std_vec_x = torch.maximum(torch.sqrt(std_vec_x/num_accs_x - mean_vec_x**2),eps)
+
+    mean_vec_edge = mean_vec_edge/num_accs_edge
+    std_vec_edge = torch.maximum(torch.sqrt(std_vec_edge/num_accs_edge - mean_vec_edge**2),eps)
+
+    mean_vec_y = mean_vec_y/num_accs_y
+    std_vec_y = torch.maximum(torch.sqrt(std_vec_y/num_accs_y - mean_vec_y**2),eps)
+
+    mean_std_list=[mean_vec_x,std_vec_x,mean_vec_edge,std_vec_edge,mean_vec_y,std_vec_y]
+
+    return mean_std_list
+
+
+
+def build_optimizer(args, params):
+    weight_decay = args.weight_decay
+    filter_fn = filter(lambda p : p.requires_grad, params)
+    if args.opt == 'adam':
+        optimizer = optim.Adam(filter_fn, lr=args.lr, weight_decay=weight_decay)
+    elif args.opt == 'sgd':
+        optimizer = optim.SGD(filter_fn, lr=args.lr, momentum=0.95, weight_decay=weight_decay)
+    elif args.opt == 'rmsprop':
+        optimizer = optim.RMSprop(filter_fn, lr=args.lr, weight_decay=weight_decay)
+    elif args.opt == 'adagrad':
+        optimizer = optim.Adagrad(filter_fn, lr=args.lr, weight_decay=weight_decay)
+    if args.opt_scheduler == 'none':
+        return None, optimizer
+    elif args.opt_scheduler == 'step':
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.opt_decay_step, gamma=args.opt_decay_rate)
+    elif args.opt_scheduler == 'cos':
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.opt_restart)
+    return scheduler, optimizer
